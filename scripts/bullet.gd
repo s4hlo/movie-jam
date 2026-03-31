911ed6641097f3def4cf371d0e85cb896c22526b
e-rick23 <erickmarques63@gmail.com>
2026-03-30 23:48:47 -0300

    adicionados sons das balas

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,11 +1,17 @@
 extends Area2D
 
 const SPEED: int = 1300
-var damage:int = 10
+var damage: int = 10
+
+# Referência ao nó de som
+@onready var som_disparo: AudioStreamPlayer2D = $SomDisparo
+
+func _ready() -> void:
+	# O som toca assim que a bala "nasce"
+	som_disparo.play()
 
 func _process(delta: float) -> void:
 	position += transform.x * SPEED * delta
-
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
