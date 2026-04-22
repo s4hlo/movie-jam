1e472a4dfdc00af97b55fa6fc59b7e1de6a00eaf
rafael magno <rafael.nunes@tarken.ag>
2026-04-23 00:36:08 +0300

    add colission to boxes

--- scripts/box.gd
+++ scripts/box.gd
@@ -1,14 +1,4 @@
-extends Area2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+extends StaticBody2D
 
 
 func _on_area_entered(area: Area2D) -> void:
